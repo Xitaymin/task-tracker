cd65bd56102096be2ef7654fb5bd8d79b09fe59a
xitaymin <y.barashenko@gmail.com>
2021-11-04 22:50:25 +0200

    added docker-compose and init script for it

--- src/main/java/com/xitaymin/tasktracker/dao/entity/User.java
+++ src/main/java/com/xitaymin/tasktracker/dao/entity/User.java
@@ -3,14 +3,22 @@ package com.xitaymin.tasktracker.dao.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
+    //todo generation types description and allocation size
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    @SequenceGenerator(name = "users_seq", sequenceName = "SEQ_USER", allocationSize = 10)
     private long id;
     @NotBlank
     private String name;
