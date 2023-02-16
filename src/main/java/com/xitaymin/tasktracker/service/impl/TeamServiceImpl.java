e3b00672f81928c8bdd9f93413729ccc705e17dc
Yaroslav Barashenko <y.barashenko@gmail.com>
2023-02-16 09:57:42 +0200

    Refactored TeamServiceImpl.java

--- src/main/java/com/xitaymin/tasktracker/service/impl/TeamServiceImpl.java
+++ src/main/java/com/xitaymin/tasktracker/service/impl/TeamServiceImpl.java
@@ -2,7 +2,7 @@ package com.xitaymin.tasktracker.service.impl;
 
 import com.xitaymin.tasktracker.dao.TeamDao;
 import com.xitaymin.tasktracker.dao.UserDAO;
-import com.xitaymin.tasktracker.dao.entity.Project;
+import com.xitaymin.tasktracker.dao.entity.BaseEntity;
 import com.xitaymin.tasktracker.dao.entity.Role;
 import com.xitaymin.tasktracker.dao.entity.Team;
 import com.xitaymin.tasktracker.dao.entity.User;
@@ -11,28 +11,24 @@ import com.xitaymin.tasktracker.dto.team.CreateTeamTO;
 import com.xitaymin.tasktracker.dto.team.EditTeamTO;
 import com.xitaymin.tasktracker.service.TeamService;
 import com.xitaymin.tasktracker.service.validators.TeamValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
 
 import static com.xitaymin.tasktracker.service.utils.EntityAbsentUtils.throwExceptionIfAbsent;
 import static com.xitaymin.tasktracker.service.validators.impl.UserValidatorImpl.USER_NOT_FOUND;
+import static java.util.stream.Collectors.toSet;
 
 @Service
+@RequiredArgsConstructor
 public class TeamServiceImpl implements TeamService {
     public static final String TEAM_NOT_FOUND = "Team with id = %d doesn't exist.";
     private final TeamDao teamDao;
     private final TeamValidator teamValidator;
     private final UserDAO userDao;
-
-    public TeamServiceImpl(TeamDao teamDao, TeamValidator teamValidator, UserDAO userDao) {
-        this.teamDao = teamDao;
-        this.teamValidator = teamValidator;
-        this.userDao = userDao;
-    }
 
     @Override
     public TeamViewTO saveTeam(CreateTeamTO createTeamTO) {
@@ -59,14 +55,10 @@ public class TeamServiceImpl implements TeamService {
 
     @Override
     public Collection<TeamViewTO> getAllTeams() {
-        Collection<Team> teams = teamDao.findAllFullTeams();
-
-        Collection<TeamViewTO> teamViewTOS = new HashSet<>();
-        for (Team team : teams) {
-            teamViewTOS.add(convertToTO(team));
-        }
-
-        return teamViewTOS;
+        return teamDao.findAllFullTeams()
+                .stream()
+                .map(this::convertToTO)
+                .collect(toSet());
     }
 
     @Transactional
@@ -90,7 +82,6 @@ public class TeamServiceImpl implements TeamService {
         teamValidator.validateForAddMember(team, user);
 
         team.addMember(user);
-
     }
 
     @Transactional
@@ -108,15 +99,17 @@ public class TeamServiceImpl implements TeamService {
     }
 
     private TeamViewTO convertToTO(Team team) {
-        Set<Long> projectsId = new HashSet<>();
-        for (Project project : team.getProjects()) {
-            projectsId.add(project.getId());
-        }
-        Set<Long> membersId = new HashSet<>();
-        for (User user : team.getMembers()) {
-            Long memberId = user.getId();
-            membersId.add(memberId);
-        }
+
+        Set<Long> projectsId = team.getProjects()
+                .stream()
+                .map(BaseEntity::getId)
+                .collect(toSet());
+
+        Set<Long> membersId = team.getMembers()
+                .stream()
+                .map(BaseEntity::getId)
+                .collect(toSet());
+
         return new TeamViewTO(team.getId(), team.getName(), membersId, projectsId);
     }
 }
