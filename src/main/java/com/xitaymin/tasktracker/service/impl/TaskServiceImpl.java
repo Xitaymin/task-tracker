8251519106b562bc0041301924f75200d29a84ac
Yaroslav Barashenko <y.barashenko@gmail.com>
2023-02-16 09:55:49 +0200

    Refactored TaskServiceImpl.java

--- src/main/java/com/xitaymin/tasktracker/service/impl/TaskServiceImpl.java
+++ src/main/java/com/xitaymin/tasktracker/service/impl/TaskServiceImpl.java
@@ -1,16 +1,14 @@
 package com.xitaymin.tasktracker.service.impl;
 
-import com.xitaymin.tasktracker.dao.ProjectDao;
 import com.xitaymin.tasktracker.dao.TaskDAO;
-import com.xitaymin.tasktracker.dao.UserDAO;
 import com.xitaymin.tasktracker.dao.entity.Task;
 import com.xitaymin.tasktracker.dao.entity.User;
 import com.xitaymin.tasktracker.dto.task.CreateTaskTO;
 import com.xitaymin.tasktracker.dto.task.EditTaskTO;
 import com.xitaymin.tasktracker.dto.task.TaskViewTO;
 import com.xitaymin.tasktracker.service.TaskService;
-import com.xitaymin.tasktracker.service.exceptions.NotFoundResourceException;
 import com.xitaymin.tasktracker.service.validators.TaskValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -19,60 +17,51 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+import static com.xitaymin.tasktracker.service.utils.EntityAbsentUtils.throwExceptionIfAbsent;
 import static com.xitaymin.tasktracker.service.validators.impl.TaskValidatorImpl.TASK_NOT_FOUND;
+import static java.util.stream.Collectors.toSet;
 
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
     private final TaskDAO taskDAO;
     private final TaskValidator taskValidator;
-    private final ProjectDao projectDao;
-    private final UserDAO userDao;
-
-
-    public TaskServiceImpl(TaskDAO taskDAO, TaskValidator taskValidator, ProjectDao projectDao, UserDAO userDao) {
-        this.taskDAO = taskDAO;
-        this.taskValidator = taskValidator;
-        this.projectDao = projectDao;
-        this.userDao = userDao;
-    }
 
     @Override
     @Transactional
     public void editTask(@Valid EditTaskTO taskTO) {
         Task task = taskDAO.findOne(taskTO.getId());
-        if (task == null) {
-            throw new NotFoundResourceException(String.format(TASK_NOT_FOUND, taskTO.getId()));
-        }
+
+        throwExceptionIfAbsent(TASK_NOT_FOUND, task, taskTO.getId());
+
         task.setTitle(taskTO.getTitle());
         task.setDescription(taskTO.getDescription());
-        taskDAO.update(task);
     }
 
     @Override
     public TaskViewTO getTask(long id) {
         Task task = taskDAO.findFullTaskById(id);
-        if (task == null) {
-            throw new NotFoundResourceException(String.format(TASK_NOT_FOUND, id));
-        } else {
-            return convertToTO(task);
-        }
+
+        throwExceptionIfAbsent(TASK_NOT_FOUND, task, id);
+
+        return convertToTO(task);
     }
 
     @Override
     public Collection<TaskViewTO> getTasks() {
-        Collection<TaskViewTO> results = new HashSet<>();
-        for (Task task : taskDAO.findAllFullTasks()) {
-            TaskViewTO to = convertToTO(task);
-            results.add(to);
-        }
-        return results;
+
+        return taskDAO.findAllFullTasks()
+                .stream()
+                .map(this::convertToTO)
+                .collect(toSet());
     }
 
     @Override
     @Transactional
     public TaskViewTO saveTask(CreateTaskTO taskTO) {
         Task task = taskValidator.getTaskValidForSave(taskTO);
+
         User assignee = task.getAssignee();
         assignee.getTasks().add(task);
 
@@ -94,6 +83,7 @@ public class TaskServiceImpl implements TaskService {
                 subtasksId.add(task.getId());
             }
         }
+
         Long parentId = null;
         Task parent = savedTask.getParent();
         if (parent != null) {
