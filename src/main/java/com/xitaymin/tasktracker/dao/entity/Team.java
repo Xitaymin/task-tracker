fd162eb937e9be59e7df38c87bdf424f6cfc8a57
Yaroslav Barashenko <y.barashenko@gmail.com>
2023-02-16 13:42:34 +0200

    Refactored Team.java

--- src/main/java/com/xitaymin/tasktracker/dao/entity/Team.java
+++ src/main/java/com/xitaymin/tasktracker/dao/entity/Team.java
@@ -1,5 +1,9 @@
 package com.xitaymin.tasktracker.dao.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,14 +14,18 @@ import javax.persistence.OneToMany;
 import java.util.HashSet;
 import java.util.Set;
 
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor
+@Getter
 @Entity
 @NamedQueries({@NamedQuery(name = Team.FIND_ALL, query = "SELECT t FROM Team t order by t.id"),
-               @NamedQuery(name = Team.FIND_TEAM_WITH_MEMBERS_AND_PROJECTS_BY_ID,
-                       query = "SELECT  t FROM Team t LEFT JOIN FETCH t.members LEFT JOIN FETCH t.projects WHERE t.id=:id"),
-               @NamedQuery(name = Team.FIND_TEAM_WITH_MEMBERS_BY_ID,
-                       query = "SELECT  t FROM Team t LEFT JOIN FETCH t.members WHERE t.id=:id"),
-               @NamedQuery(name = Team.FIND_ALL_TEAMS_WITH_MEMBERS,
-                       query = "SELECT DISTINCT t FROM Team t LEFT JOIN FETCH t.members")})
+        @NamedQuery(name = Team.FIND_TEAM_WITH_MEMBERS_AND_PROJECTS_BY_ID,
+                query = "SELECT  t FROM Team t LEFT JOIN FETCH t.members LEFT JOIN FETCH t.projects WHERE t.id=:id"),
+        @NamedQuery(name = Team.FIND_TEAM_WITH_MEMBERS_BY_ID,
+                query = "SELECT  t FROM Team t LEFT JOIN FETCH t.members WHERE t.id=:id"),
+        @NamedQuery(name = Team.FIND_ALL_TEAMS_WITH_MEMBERS,
+                query = "SELECT DISTINCT t FROM Team t LEFT JOIN FETCH t.members")})
 public class Team extends BaseEntity {
 
     public static final String FIND_ALL = "Team.findAll";
@@ -25,49 +33,22 @@ public class Team extends BaseEntity {
     public static final String FIND_TEAM_WITH_MEMBERS_BY_ID = "Team.findTeamWithMembersById";
     public static final String FIND_ALL_TEAMS_WITH_MEMBERS = "Team.findAllTeamsWithMembers";
 
+    @Setter
     @Column(nullable = false)
     private String name;
+    @Setter(value = PRIVATE)
     @OneToMany(mappedBy = "team", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Set<User> members = new HashSet<>();
-
+    @Setter(value = PRIVATE)
     @ManyToMany(mappedBy = "teams")
     private Set<Project> projects = new HashSet<>();
 
-    public Team() {
-    }
-
     public Team(String name) {
         this.name = name;
-    }
-
-    public Set<Project> getProjects() {
-        return projects;
-    }
-
-    private void setProjects(Set<Project> projects) {
-        this.projects = projects;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<User> getMembers() {
-        return members;
-    }
-
-    private void setMembers(Set<User> members) {
-        this.members = members;
     }
 
     public void addMember(User member) {
         this.getMembers().add(member);
         member.setTeam(this);
     }
-
-
 }
